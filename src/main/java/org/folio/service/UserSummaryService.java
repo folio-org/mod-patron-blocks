aaee35b37e3a97d39640bd0a8dcb82d7e56c8ea3
Sergiy Bichurin <sergiy_bichurin@epam.com>
2022-04-11 14:53:58 +0300

    Remove code smell

--- src/main/java/org/folio/service/UserSummaryService.java
+++ src/main/java/org/folio/service/UserSummaryService.java
@@ -297,7 +297,7 @@ public class UserSummaryService {
       .ifPresentOrElse(openLoan -> {
         openLoan.setDueDate(event.getDueDate());
         openLoan.setRecall(event.getDueDateChangedByRecall());
-        if(!event.getDueDateChangedByRecall()){
+        if(Boolean.FALSE.equals(event.getDueDateChangedByRecall())){
           openLoan.setItemLost(false);
         }
       }, () -> logOpenLoanNotFound(event, event.getLoanId()));
